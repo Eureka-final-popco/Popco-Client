225d614f120f08d77c3164f8451b557efa273937
guntaek <rjsxor7584@naver.com>
2025-07-10 11:33:26 +0900

    chore: ContentReactionRepository 로 파일 이름 수정

--- src/main/java/com/popcoclient/content/repository/ContentReactionRepository.java
+++ src/main/java/com/popcoclient/content/repository/ContentReactionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollectionReactionRepository extends JpaRepository<ContentReaction, Long> {
+public interface ContentReactionRepository extends JpaRepository<ContentReaction, Long> {
 }
