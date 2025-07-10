ceb69186cdb9a26a7431e696490c67939c59dcc3
guntaek <rjsxor7584@naver.com>
2025-07-10 11:30:24 +0900

    chore: 오타 수정

--- src/main/java/com/popcoclient/content/entity/ContentReaction.java
+++ src/main/java/com/popcoclient/content/entity/ContentReaction.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Table(name = "Content_reaction")
+@Table(name = "content_reaction")
 @Entity
 @Builder
 @Getter
