7f2ca41ae48a982399de6a57b7b03a8ca2a33842
Jimin Byun <fraise3838@naver.com>
2025-07-10 11:52:24 +0900

    fix : @CreatedDate, @LastModifiedDate로 변경

--- src/main/java/com/popcoclient/content/entity/ContentActor.java
+++ src/main/java/com/popcoclient/content/entity/ContentActor.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -31,11 +33,11 @@ public class ContentActor {
     @Column(nullable = false)
     private Integer order;
 
-    @CreationTimestamp
+    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
