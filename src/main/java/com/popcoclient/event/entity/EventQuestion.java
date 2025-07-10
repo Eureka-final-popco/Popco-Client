d1fa491ab1c5af3b09fd4254e0184de49bccfa6e
Jimin Byun <fraise3838@naver.com>
2025-07-10 10:48:58 +0900

    fix : @CreatedDate, @LastModifiedDate로 변경

--- src/main/java/com/popcoclient/event/entity/EventQuestion.java
+++ src/main/java/com/popcoclient/event/entity/EventQuestion.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -36,11 +38,11 @@ public class EventQuestion {
     @Column(name = "img_path")
     private String imgPath;
 
-    @CreationTimestamp
+    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
