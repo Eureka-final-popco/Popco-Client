c678cd86928ba100455a2c94c7e546fedf838f55
hyew0nn <hyewon-0607@naver.com>
2025-07-28 17:38:13 +0900

    chore: 신고 controller SecurityRequirement 추가

--- src/main/java/com/popcoclient/declaration/controller/DeclarationController.java
+++ src/main/java/com/popcoclient/declaration/controller/DeclarationController.java
@@ -7,6 +7,7 @@ import com.popcoclient.declaration.dto.response.DeclarationTypeResponseDto;
 import com.popcoclient.declaration.service.DeclarationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +33,7 @@ public class DeclarationController {
     }
 
     @Operation(summary = "리뷰 신고 생성", description = "특정 리뷰에 대해 신고를 생성합니다.")
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/reviews/{reviewId}")
     public ResponseEntity<ApiResponse<Void>> createDeclaration(@PathVariable Long reviewId,
                                                                @Valid @RequestBody DeclarationCreateRequestDto dto) {
