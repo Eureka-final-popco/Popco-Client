3d032f646f3c2bc3ddfd0218646579e5ae62f564
hyew0nn <hyewon-0607@naver.com>
2025-08-01 14:46:57 +0900

    fix: 코드레빗 코드리뷰 수정

--- src/main/java/com/popcoclient/exception/business/QuizNotFoundForTodayException.java
+++ src/main/java/com/popcoclient/exception/business/QuizNotFoundForTodayException.java
@@ -5,10 +5,10 @@ import com.popcoclient.exception.ErrorCode;
 
 public class QuizNotFoundForTodayException extends BusinessException {
   public QuizNotFoundForTodayException() {
-    super(ErrorCode.QUIZ_ID_MISMATCH_FOR_TODAY);
+    super(ErrorCode.QUIZ_NOT_FOUND_FOR_TODAY);
   }
 
   public QuizNotFoundForTodayException(String message) {
-    super(ErrorCode.QUIZ_ID_MISMATCH_FOR_TODAY, message);
+    super(ErrorCode.QUIZ_NOT_FOUND_FOR_TODAY, message);
   }
 }
