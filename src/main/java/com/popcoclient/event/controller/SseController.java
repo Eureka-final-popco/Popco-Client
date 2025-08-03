d9bdb3fc230ebaf7dbf0acd10b435fdda74ffd76
Gihwan-Park <pgh5121@naver.com>
2025-08-03 20:04:20 +0900

    chore : 명세 작성

--- src/main/java/com/popcoclient/event/controller/SseController.java
+++ src/main/java/com/popcoclient/event/controller/SseController.java
@@ -4,6 +4,8 @@ import com.popcoclient.common.response.ApiResponse;
 import com.popcoclient.event.dto.response.ConnectionStatusResponseDto;
 import com.popcoclient.event.service.impl.SseNotificationManagementService;
 import com.popcoclient.event.service.impl.SseNotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +19,14 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
+@Tag(name = "SSE", description = "알림 관련 API")
 @Slf4j
 @CrossOrigin(origins = "*") // 개발용, 운영에서는 구체적인 도메인 지정
 public class SseController {
 
     private final SseNotificationManagementService sseManagementService;
 
+    @Operation(summary = "SSE 채널 구독 API", description = "프론트에서 new EventSource('/notifications/stream') 으로 이벤트 서버 연결, 이후 송신은 자동처리됨")
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter streamNotifications(
             @RequestParam(required = false) String clientId,
@@ -31,6 +35,7 @@ public class SseController {
         return sseManagementService.createSseConnection(clientId, request);
     }
 
+    @Operation(summary = "SSE 테스트 및 모니터링", description = "SSE 상태를 간단히 테스트해볼 수 있는 Api, 상태 파악 가능, healthCheck")
     @GetMapping("/status")
     public ResponseEntity<ApiResponse<ConnectionStatusResponseDto>> getConnectionStatus() {
         ConnectionStatusResponseDto status = sseManagementService.getConnectionStatus();
